5d84da527277f735868f4f6b453a1092b0bbeaac
fauzisaja <uziwild@gmail.com>
2023-01-08 05:27:29 +0800

    perubahan nama class pada transaksi list

--- transaksi_list.php
+++ transaksi_list.php
@@ -77,7 +77,7 @@
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">23pmcafe</a>
+          <a href="#" class="d-block">Muhammad Fauzi</a>
         </div>
       </div>
 
